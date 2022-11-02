a5010a878efb68c20e6cf5712c72c19f211244bb
alvarohsp <alvaro.hpandelo@gmail.com>
2022-11-02 20:10:27 -0300

    Hiding DevTools

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const createWindow = () => {
 
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
   mainWindow.removeMenu();
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 const changePage = (pageUrl: string) => {
