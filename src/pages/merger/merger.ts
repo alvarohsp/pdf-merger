b62e257ffd04de05ae2512aacf336d21856d5efb
alvarohsp <alvaro.hpandelo@gmail.com>
2022-11-05 19:52:47 -0300

    Added delete and page order buttons, v0.6.0

--- src/pages/merger/merger.ts
+++ src/pages/merger/merger.ts
@@ -16,27 +16,77 @@ export function onClickBrowse(): void {
   window.baseAPI.invoke('browseFiles').then((res: FileInfo[]) => {
     if (res?.length > 0) {
       fileList.push(...res);
-      clearListElement.classList.remove('hide');
-      saveListElement.classList.remove('hide');
+      hideShowButtons(true);
     }
     updateView();
   });
 }
 
 function updateView(): void {
+  if (fileList.length === 0) {
+    clearFileList();
+    return;
+  }
   clearFileListElement();
-  fileList.forEach((element) => {
-    drawFile(element);
+  fileList.forEach((element, index) => {
+    drawFile(element, index);
   });
 }
 
-function drawFile(element: FileInfo): void {
+function drawFile(element: FileInfo, index: number): void {
   const fileElement = document.createElement('file-block');
   fileElement.setAttribute('fileName', element.name);
   fileElement.setAttribute('fileSize', element.size);
   fileElement.setAttribute('extension', element.extension);
+  fileElement.setAttribute(
+    'last',
+    `${index === fileList.length - 1}`
+  );
+  fileElement.setAttribute('index-pos', `${index}`);
+  fileElement.setAttribute(
+    'delete-clb',
+    `${callbackDeleteFile.name}`
+  );
+  fileElement.setAttribute(
+    'next-clb',
+    `${callbackNextPosition.name}`
+  );
+  fileElement.setAttribute(
+    'back-clb',
+    `${callbackPreviousPosition.name}`
+  );
 
   fileListElement.appendChild(fileElement);
+}
+
+function callbackDeleteFile(position: string): void {
+  const numericPosition = Number(position);
+
+  fileList.splice(numericPosition, 1);
+
+  updateView();
+}
+
+function callbackNextPosition(position: string): void {
+  const numericPos = Number(position);
+
+  [fileList[numericPos], fileList[numericPos + 1]] = [
+    fileList[numericPos + 1],
+    fileList[numericPos],
+  ];
+
+  updateView();
+}
+
+function callbackPreviousPosition(position: string): void {
+  const numericPos = Number(position);
+
+  [fileList[numericPos - 1], fileList[numericPos]] = [
+    fileList[numericPos],
+    fileList[numericPos - 1],
+  ];
+
+  updateView();
 }
 
 function clearFileListElement(): void {
@@ -46,8 +96,17 @@ function clearFileListElement(): void {
 function clearFileList(): void {
   fileList = [];
   clearFileListElement();
-  clearListElement.classList.add('hide');
-  saveListElement.classList.add('hide');
+  hideShowButtons(false);
+}
+
+function hideShowButtons(show: boolean): void {
+  if (show) {
+    clearListElement.classList.remove('hide');
+    saveListElement.classList.remove('hide');
+  } else {
+    clearListElement.classList.add('hide');
+    saveListElement.classList.add('hide');
+  }
 }
 
 export function saveFileList(): void {
--- src/shared/components/file-block/file-block.ts
+++ src/shared/components/file-block/file-block.ts
@@ -5,6 +5,7 @@ class FileBlock extends HTMLElement {
   connectedCallback() {
     const fileElement = document.createElement('div');
     fileElement.classList.add('dv-file-block');
+
     const svgElement = document.createElement('svg');
     svgElement.setAttribute('width', '64');
     svgElement.setAttribute('height', '64');
@@ -22,6 +23,10 @@ class FileBlock extends HTMLElement {
 
     const fileInfoElement = document.createElement('div');
     fileInfoElement.classList.add('file-info');
+
+    const insideFileInfoElement = document.createElement('div');
+    insideFileInfoElement.classList.add('dv-file-group');
+
     const fileNameElement = document.createElement('p');
     fileNameElement.classList.add('file-name');
     fileNameElement.innerText = this.getAttribute('fileName');
@@ -30,11 +35,81 @@ class FileBlock extends HTMLElement {
     fileSizeElement.classList.add('file-size');
     fileSizeElement.innerText = this.getAttribute('fileSize');
 
-    fileInfoElement.appendChild(fileNameElement);
-    fileInfoElement.appendChild(fileSizeElement);
+    insideFileInfoElement.appendChild(fileNameElement);
+    insideFileInfoElement.appendChild(fileSizeElement);
+
+    fileInfoElement.appendChild(insideFileInfoElement);
+
+    const insideFileInfoBtns = document.createElement('div');
+    insideFileInfoBtns.classList.add('dv-file-btn');
+
+    //////////////////////////////////////
+    // CREATING DELETE BUTTON
+
+    const insideFileInfoBtnBar1 = document.createElement('div');
+    insideFileInfoBtnBar1.classList.add('dv-btn-bar');
+
+    const imageBtn1 = document.createElement('img');
+    imageBtn1.src = getImageByBtn('cancel');
+    imageBtn1.classList.add('p-btn-bar');
+
+    imageBtn1.onclick = () => {
+      const arg = this.getAttribute('index-pos');
+      const clb = this.getAttribute('delete-clb');
+      eval(`${clb}('${arg}')`);
+    };
+
+    insideFileInfoBtnBar1.appendChild(imageBtn1);
+
+    insideFileInfoBtns.appendChild(insideFileInfoBtnBar1);
+
+    //////////////////////////////////////
+    // CREATING NEXT BUTTON
+
+    if (this.getAttribute('last') === 'false') {
+      const insideFileInfoBtnBar2 = document.createElement('div');
+      insideFileInfoBtnBar2.classList.add('dv-btn-bar');
+
+      const imageBtn2 = document.createElement('img');
+      imageBtn2.src = getImageByBtn('next');
+      imageBtn2.classList.add('p-btn-bar');
+
+      imageBtn2.onclick = () => {
+        const arg = this.getAttribute('index-pos');
+        const clb = this.getAttribute('next-clb');
+        eval(`${clb}('${arg}')`);
+      };
+
+      insideFileInfoBtnBar2.appendChild(imageBtn2);
+
+      insideFileInfoBtns.appendChild(insideFileInfoBtnBar2);
+    }
+
+    //////////////////////////////////////
+    // CREATING BACK BUTTON
+
+    if (this.getAttribute('index-pos') !== '0') {
+      const insideFileInfoBtnBar3 = document.createElement('div');
+      insideFileInfoBtnBar3.classList.add('dv-btn-bar');
+
+      const imageBtn3 = document.createElement('img');
+      imageBtn3.src = getImageByBtn('back');
+      imageBtn3.classList.add('p-btn-bar');
+
+      imageBtn3.onclick = () => {
+        const arg = this.getAttribute('index-pos');
+        const clb = this.getAttribute('back-clb');
+        eval(`${clb}('${arg}')`);
+      };
+
+      insideFileInfoBtnBar3.appendChild(imageBtn3);
+
+      insideFileInfoBtns.appendChild(insideFileInfoBtnBar3);
+    }
+
+    fileInfoElement.appendChild(insideFileInfoBtns);
 
     fileElement.appendChild(fileInfoElement);
-
     this.appendChild(fileElement);
   }
 }
@@ -49,6 +124,17 @@ function getImageByExtension(extension: string): string {
 
     case 'png':
       return '../../../src/assets/svg/png_file_icon.svg';
+  }
+}
+
+function getImageByBtn(button: string): string {
+  switch (button) {
+    case 'cancel':
+      return '../../../src/assets/button/btn-cancel.png';
+    case 'next':
+      return '../../../src/assets/button/btn-next.png';
+    case 'back':
+      return '../../../src/assets/button/btn-back.png';
   }
 }
 
